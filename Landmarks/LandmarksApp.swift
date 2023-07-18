d86c76c38b79018b9e675be76c2c14ca09d61f3e
Hiroki Minami <hirokimnm6@gmail.com>
2023-07-18 14:07:01 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hiroki Minami on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
