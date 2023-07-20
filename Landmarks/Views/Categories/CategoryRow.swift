079ee82da398b36bc5f494e7048f5938cf9e254f
Hiroki Minami <hirokimnm6@gmail.com>
2023-07-20 16:53:36 -0700

    done: tutorial

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,45 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Hiroki Minami on 2023-07-19.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+  var categoryName: String
+  var items: [Landmark]
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text(categoryName)
+        .font(.headline)
+        .padding(.leading, 15)
+        .padding(.top, 5)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(alignment: .top, spacing: 0) {
+          ForEach(items) { landmark in
+            NavigationLink {
+              LandmarkDetail(landmark: landmark)
+            } label: {
+              CategoryItem(landmark: landmark)
+            }
+          }
+        }
+      }
+      .frame(height: 185)
+      
+    }
+  }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+  static var landmakrs = ModelData().landmarks
+  
+  static var previews: some View {
+    CategoryRow(
+      categoryName: landmakrs[0].category.rawValue, items: Array(landmakrs.prefix(4)))
+  }
+}
---
+++ MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUIView.swift
+//  MacLandmarks
+//
+//  Created by Hiroki Minami on 2023-07-20.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+  @AppStorage("MapView.zoom")
+  private var zoom: MapView.Zoom = .medium
+  
+  var body: some View {
+    Form {
+      Picker("Map Zoom:", selection: $zoom) {
+        ForEach(MapView.Zoom.allCases) { level in
+          Text(level.rawValue)
+        }
+      }
+      .pickerStyle(.inline)
+    }
+    .frame(width: 300)
+    .navigationTitle("Landmark Settings")
+    .padding(80)
+  }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+  static var previews: some View {
+    LandmarkSettings()
+  }
+}
